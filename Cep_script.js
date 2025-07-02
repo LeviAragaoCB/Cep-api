3a7741887139e603d1c98b9b1bffec5a7ebeac2e
LEVI ARAGAO DE CASTRO BERNARDO <levibernardo0753@digitalcollege.internal>
2025-07-01 22:07:21 -0300

    API do IBGE

--- Cep_script.js
+++ Cep_script.js
@@ -14,5 +14,46 @@
         document.getElementById('bairro').value = json.district
         document.getElementById('ddd').value = json.ddd
         document.getElementById('uf').value = json.state
+        document.getElementById('localidade').value = json.city
     })
 }
+
+const fetchEstados = () => {
+  let uriEstados = `https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome`
+
+  console.log(`URI: ${uriEstados}`)
+  fetch(uriEstados)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+
+        let options = `<option select disable>Selecione...</option>`
+
+        data.forEach(estado => {
+          options += `<option value="${estado.sigla}">${estado.nome}</option>`
+        });
+
+        document.getElementById('uf').innerHTML = options
+      })
+}
+fetchEstados()
+
+const fetchCidades = () => {
+  let uriCidades = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/CE/municipios`
+
+  console.log(`URI: ${uriCidades}`)
+  fetch(uriCidades)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+
+        let options = `<option select disable>Selecione...</option>`
+
+        data.forEach(cidade => {
+          options += `<option value="${cidade.nome}">${cidade.nome}</option>`
+        });
+
+        document.getElementById('localidade').innerHTML = options
+      })
+}
+fetchCidades()
